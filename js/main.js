4cff4dd4598f089034cda172f7147195c3f24e08
Andrey Danilov <polovmenter840@gmail.com>
2024-10-05 22:48:27 +0300

    Добавил анимации

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const burgerButton = document.querySelector(".burger-button")
 const closeButton = document.querySelector(".mobile-menu__close-button")
 const mobileMenu = document.querySelector(".mobile-menu")
+const scrollItems = document.querySelectorAll(".scroll-item")
+let offset = 110;
 
 burgerButton.addEventListener("click", () => {
     mobileMenu.classList.add("mobile-menu--open")
@@ -8,4 +10,33 @@ burgerButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     mobileMenu.classList.remove("mobile-menu--open")
+})
+
+const elementInView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - offset );
+}
+
+const displayScrollElement = (el) => {
+    el.classList.add("scrolled")
+}
+
+const hideScrollElemet = (el) => {
+    el.classList.remove("scrolled")
+}
+
+const handleScrollAnimation = () => {
+    scrollItems.forEach((el) => {
+        if (elementInView(el)) {
+            displayScrollElement(el)
+        }
+        else {
+            hideScrollElemet(el)
+        }
+    })
+}
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
 })
